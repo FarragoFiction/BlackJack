04e1dbee9e70778794579abd72ac4d8a8785388a
Jenny <gtg287y@gmail.com>
2018-04-21 10:26:40 -0400

    so streamlined

--- example/main.dart
+++ example/main.dart
@@ -11,44 +11,10 @@ main() {
 
 Future<Null> start() async{
     await Loader.preloadManifest();
-    container = querySelector("#output");
-    List<Card> cards = Card.getFreshDeck();
-    cards = Card.shuffleDeck(cards);
-
-    game = new Game(cards,container, finishGame);
+    game = new Game(Card.getFreshDeck(),querySelector("#output"), finishGame);
     game.start();
 }
 
 void finishGame() {
-    window.alert("game is over");
-}
-
-
-
-void displayStartGame() {
-    game.player.render();
-    game.dealer.render();
-}
-
-void testHand() {
-    DivElement d = new DivElement();
-    container.append(d);
-    d.text = "${game.deck[0].name}";
-
-    DivElement d2 = new DivElement();
-    container.append(d2);
-    d2.text = "${game.deck[1].name}";
-
-    DivElement d3 = new DivElement();
-    container.append(d3);
-    d3.text = "Value: ${Card.sumCards(<Card>[game.deck[0], game.deck[1]])}";
-}
-
-void testThingy() {
-    for(Card c in game.deck) {
-        DivElement d = new DivElement();
-        container.append(d);
-        d.text = "${c.name}";
-
-    }
+    window.alert("TODO: process bets");
 }
